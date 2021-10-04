b109aba64f8a6e35e160dd6bec6cf3b94e83d993
SogoudAhmed <88768395+SogoudAhmed@users.noreply.github.com>
2021-10-04 17:42:03 +0200

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,24 @@
+<?php
+$num1 = 5;
+$num2 = 5;
+$operator = '+';
+
+switch($operator){
+    case '+':
+        echo($num + $num2);
+        break;
+
+    case '*':
+        echo($num * $num2);
+        break;
+    case '-':
+        echo($num - $num2);
+        break;
+    case '/':
+         echo($num / $num2);
+         break;
+             
+}
+
+
+?>
